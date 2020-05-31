4b7db8572852541eb8156948f339182432c22b4b
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-05-31 13:19:01 -0500

    removed auto-check to allow users to select criteria

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,3 +1,17 @@
+// This is to activate the side nav.
 $(document).ready(function () {
 	$(".sidenav").sidenav();
 });
+
+// This is to start toggling through the criteria questions. 
+if (i === 3) {
+	clearInterval(timerInterval);
+	quizcontentDiv.classList.toggle("collapse");
+	quizscoreDiv.classList.toggle("collapse");
+	activeDiv = quizscoreDiv;
+	finalScore.innerHTML = secondsLeft;
+	timerNav.innerHTML = secondsLeft;
+} else {
+	i = i + 1;
+	loadQuestion(i);
+}
